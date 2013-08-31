6b37c277fc6b5bbc22a21fb3810819b68ef598da
Chris Allen <cma@bitemyapp.com>
2013-08-30 17:47:49 -0700

    marginalia, alembic

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -7,10 +7,12 @@
                   [jonase/eastwood "0.0.2"]
                   [lein-kibit "0.0.8"]
                   [com.aphyr/prism "0.1.1"]
+                  [lein-marginalia "0.7.1"]
                   [lein-cloverage "1.0.2"]]
         :datomic {:install-location "~/code/datomic-free-0.8.4138"}
         :aliases {"eval" ["run" "-m" "clojure.main/main" "-e"]}
         :dependencies [[ritz/ritz-nrepl-middleware "0.7.0"]
+                       [alembic "0.2.0"]
                        [com.cemerick/pomegranate "0.2.0"]
                        [com.aphyr/prism "0.1.1"]
                        [jark "0.4.3"]]}}
