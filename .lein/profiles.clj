e25fe89073236cbaee0eed7390cbed88eceee619
Chris Allen <cma@bitemyapp.com>
2013-09-05 11:13:48 -0700

    lein-typed

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -7,6 +7,7 @@
                   [jonase/eastwood "0.0.2"]
                   [lein-kibit "0.0.8"]
                   [com.aphyr/prism "0.1.1"]
+                  [lein-typed "0.3.0"]
                   [lein-marginalia "0.7.1"]
                   [lein-cloverage "1.0.2"]]
         :datomic {:install-location "~/code/datomic-free-0.8.4138"}
