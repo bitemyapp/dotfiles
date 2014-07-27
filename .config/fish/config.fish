0b023aa1122210e349eceb3d328d2299f5d0e01f
Chris Allen <cma@bitemyapp.com>
2014-07-27 01:32:48 -0500

    added fish config and olliolli

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+set fish_greeting
+
+alias t  "tree --dirsfirst"
+alias ll "ls -alGh"
+
+# Git aliases
+alias gs  "git status -sb"
+alias ga  "git add"
+alias gbr "git branch"
+alias gc  "git commit"
+alias gcm "git commit -m"
+alias gd  "git diff"
+alias gco "git checkout"
+alias gh  "git h"
+
+# Load paths
+set PATH $PATH /usr/local/bin
+set PATH $PATH /usr/local/sbin
+set PATH $PATH /usr/bin
+set PATH $PATH /usr/sbin
+set PATH $PATH /bin
+set PATH $PATH /sbin
+set PATH $PATH /opt/bin
+set PATH $PATH /usr/X11/bin
+set PATH $PATH $HOME/bin
+set PATH $PATH $HOME/.cabal/bin
+
+set -x LANG en_US.UTF-8
