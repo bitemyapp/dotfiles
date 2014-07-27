36a1a047dce32e9962a88e4268dafff52047b85d
Chris Allen <cma@bitemyapp.com>
2014-07-27 10:26:23 -0500

    fish prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,4 +24,26 @@ set PATH $PATH /usr/X11/bin
 set PATH $PATH $HOME/bin
 set PATH $PATH $HOME/.cabal/bin
 
+# fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch pink
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡ '
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+
+function fish_prompt
+       set last_status $status
+       set_color $fish_color_cwd
+       printf '%s' (prompt_pwd)
+       set_color normal
+       printf '%s ' (__fish_git_prompt)
+       set_color normal
+end
+
 set -x LANG en_US.UTF-8
