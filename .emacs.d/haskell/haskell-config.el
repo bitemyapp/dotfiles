8c5a6d5e021b88ada28083cc26a08d8708c7ab4c
Chris Allen <cma@bitemyapp.com>
2015-05-20 13:48:04 -0500

    cleanup

--- .emacs.d/haskell/haskell-config.el
+++ .emacs.d/haskell/haskell-config.el
@@ -5,48 +5,16 @@
 
 (require 'haskell-mode)
 (require 'hindent)
-;; (require 'haskell-process)
-;; (require 'haskell-simple-indent)
 (require 'haskell)
 (require 'haskell-indentation)
 (require 'haskell-interactive-mode)
+(require 'haskell-process)
 (require 'haskell-font-lock)
 
 (setq auto-mode-alist (cons '("\.hs$" . haskell-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\.lhs$" . tex-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\.purs$" . haskell-mode) auto-mode-alist))
 (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
-
-;; (require 'ghc)
-;; (autoload 'ghc-init "ghc" nil t)
-;; (autoload 'ghc-debug "ghc" nil t)
-;; (add-hook 'haskell-mode-hook 'ghc-init)
-;; (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-
-;; (defun ghc-jump-to-def (&optional ask)
-;;   (interactive "P")
-;;   (let* ((expr0 (ghc-things-at-point))
-;;          (expr (if (or ask (not expr0)) (ghc-read-expression expr0) expr0))
-;;          (info (ghc-get-info expr)))
-;;     (when (and info
-;;                (not (string= info "Cannot show info")))
-;;       (let* ( (re "Defined at \\([^:]+\\):\\([[:digit:]]+\\):\\([[:digit:]]+\\)")
-;;               (filename (progn (string-match re info)
-;;                                (match-string 1 info)))
-
-;;               (row (progn (string-match re info)
-;;                           (match-string 2 info)))
-
-;;               (col (progn (string-match re info)
-;;                           (match-string 3 info))))
-
-;;         (find-file filename)
-;;         (goto-line (string-to-number row))
-;;         (beginning-of-line)
-;;         (forward-char (string-to-number col))))))
-
-;; (defvar ghc-jumpdef-key "\C-c\C-d")
-;; (define-key haskell-mode-map ghc-jumpdef-key 'ghc-jump-to-def)
 
 (custom-set-variables 
  '(haskell-process-type (quote cabal-repl))
@@ -61,7 +29,7 @@
  '(haskell-process-use-presentation-mode t)
  '(haskell-interactive-mode-include-file-name nil)
  '(haskell-interactive-mode-eval-pretty nil)
- '(haskell-process-do-cabal-format-string ":!cd %s && unset GHC_PACKAGE_PATH && %s")
+ ;; '(haskell-process-do-cabal-format-string ":!cd %s && unset GHC_PACKAGE_PATH && %s")
  '(shm-use-hdevtools t)
  '(shm-use-presentation-mode t)
  '(shm-auto-insert-skeletons t)
@@ -69,14 +37,14 @@
  '(haskell-process-show-debug-tips nil)
  '(haskell-process-suggest-hoogle-imports nil)
  '(haskell-process-suggest-haskell-docs-imports t)
- '(hindent-style "johan-tibell"))
+ '(hindent-style "gibiansky"))
 
 (setq haskell-interactive-mode-eval-mode 'haskell-mode)
 
 (setq haskell-process-path-ghci "ghci")
 (setq haskell-process-args-ghci '("-ferror-spans"))
 
-;; (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-goto-loc)
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
 (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-tag-find)
 (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-mode-show-type-at)
 (define-key haskell-mode-map (kbd "C-?") 'haskell-mode-find-uses)
@@ -84,15 +52,6 @@
 (define-key haskell-mode-map (kbd "C-i") 'hindent-reformat-buffer)
 (define-key haskell-mode-map (kbd "C-<return>") 'haskell-simple-indent-newline-indent)
 (add-hook 'haskell-mode-hook #'hindent-mode)
-
-;; (setq ns-right-alternate-modifier nil)
-;; (setq debug-on-error t)
-
-;; (add-hook 'haskell-mode-hook 'haskell-interactive-mode)
-;; (add-hook 'haskell-mode-hook 'haskell-auto-insert-module-template)
-;; (add-hook 'w3m-display-hook 'w3m-haddock-display)
-
-;; (global-set-key (kbd "C-c c c i") 'ghc-check-insert-from-warning)
 
 ;; (require 'company)
 ;; (add-hook 'haskell-mode-hook 'company-mode)
