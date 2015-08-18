1582c9cdf31e03ed53c1354adc3805b23146b14c
Chris Allen <cma@bitemyapp.com>
2015-08-18 18:21:48 -0500

    screen tmux, yes

--- .install.sh
+++ .install.sh
@@ -5,7 +5,9 @@ sudo add-apt-repository -y ppa:git-core/ppa
 sudo add-apt-repository -y ppa:ubuntu-elisp/ppa
 sudo add-apt-repository -y ppa:hvr/ghc
 
-sudo apt-get install xmonad xmobar texlive-full texlive-xetex git suckless-tools zsh xorg-dev emacs-snapshot emacs-snapshot-el xtrlock xbacklight stalonetray fdpowermon pasystray python-pygments mosh xorg-dev
+sudo apt-get update
+
+sudo apt-get install -y xmonad xmobar texlive-full texlive-xetex git suckless-tools zsh xorg-dev emacs-snapshot emacs-snapshot-el xtrlock xbacklight stalonetray fdpowermon pasystray python-pygments mosh xorg-dev tmux screen
 
 wget -q -O- https://s3.amazonaws.com/download.fpcomplete.com/ubuntu/fpco.key | sudo apt-key add -
 echo 'deb http://download.fpcomplete.com/ubuntu/vivid stable main' | sudo tee /etc/apt/sources.list.d/fpco.list
@@ -13,8 +15,8 @@ sudo apt-get update && sudo apt-get install stack -y
 
 
 sudo apt-get update
-sudo apt-get install ghc-7.10.2
-sudo apt-get install cabal-install-1.22
+sudo apt-get install -y ghc-7.10.2
+sudo apt-get install -y cabal-install-1.22
 
 sudo ln -sf /opt/ghc/7.10.2/bin/ghc /usr/bin/ghc
 sudo ln -sf /opt/ghc/7.10.2/bin/ghci /usr/bin/ghci
