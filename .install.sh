d25e4805b282e5f92d5d9e87251edc9c8ba219dd
Chris Allen <cma@bitemyapp.com>
2017-11-21 19:40:32 -0600

    Fixing up install scripts

--- .install.sh
+++ .install.sh
@@ -11,7 +11,7 @@ sudo apt-get update
 sudo apt-get install -y xmonad xmobar texlive-full texlive-xetex git suckless-tools zsh emacs-snapshot emacs-snapshot-el xtrlock xbacklight stalonetray fdpowermon pasystray python-pygments mosh tmux screen htop silversearcher-ag mumble darcs libtinfo-dev inotify-tools colordiff fonts-roboto postgresql-9.5 postgresql-server-dev-9.5 libpq-dev libmysqlclient-dev libpcre3-dev cmake gimp audacity libav-tools normalize-audio libavcodec-extra curl libcurl4-openssl-dev libavcodec-dev libavdevice-dev libvulkan1
 
 # SDL deps
-sudo apt-get install libsdl2-dev libsdl2-image-dev libsdl2-gfx-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev
+sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-gfx-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev
 
 wget -q -O- https://s3.amazonaws.com/download.fpcomplete.com/ubuntu/fpco.key | sudo apt-key add -
 echo 'deb http://download.fpcomplete.com/ubuntu/vivid stable main' | sudo tee /etc/apt/sources.list.d/fpco.list
@@ -30,7 +30,7 @@ sudo ln -sf /opt/ghc/7.10.3/bin/runhaskell /usr/bin/runhaskell
 
 sudo ln -sf /opt/cabal/1.22/bin/cabal /usr/bin/cabal
 
-sudo apt-get install libghc-x11-xft-dev libasound2-dev libiw-dev libghc-libxml-sax-dev c2hs
+sudo apt-get install -y libghc-x11-xft-dev libasound2-dev libiw-dev libghc-libxml-sax-dev c2hs
 cabal update
 cabal install xmonad xmonad-contrib xmonad-extras
 
