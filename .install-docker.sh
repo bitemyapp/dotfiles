f040a5625cc80ff31a6d60cc31b5e059b9fcc565
Chris Allen <cma@bitemyapp.com>
2021-02-09 00:34:27 -0600

    Fixed docker install

--- .install-docker.sh
+++ .install-docker.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install apt-transport-https ca-certificates curl software-properties-common gnupg-agent
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu fossa stable"
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
 sudo apt update
 
-apt-cache policy docker-ce
-
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose
 
 sudo usermod -aG docker ${USER}
 
