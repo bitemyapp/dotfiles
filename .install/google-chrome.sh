2e30f90684df0684890d17e95324bf5770f3fb62
Chris Allen <cma@bitemyapp.com>
2024-06-09 12:36:42 -0500

    Avoid redundancy and KISS

--- .install/google-chrome.sh
+++ .install/google-chrome.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if dpkg -s google-chrome-beta; then
+if dpkg -s google-chrome; then
     exit 0;
 fi
 
@@ -12,5 +12,5 @@ if ! test -f /etc/apt/sources.list.d/google-chrome.list; then
     sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 fi
 
-sudo apt update && sudo apt install -y google-chrome-beta
+sudo apt update && sudo apt install -y google-chrome
 
