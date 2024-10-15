71448183e4145ff6efb0f1c4d130ef81e3a34598
Chris Allen <cma@bitemyapp.com>
2024-10-15 15:54:32 -0500

    Ensure group membership when installing Docker

--- .install/docker.sh
+++ .install/docker.sh
@@ -15,3 +15,8 @@ echo \
 sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+docker run hello-world
